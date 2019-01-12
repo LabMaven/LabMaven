679bcee1eb8c6cc8188cd937ff98b619ee0c5c9f
tobetorrent <313186950@qq.com>
2019-01-12 21:18:26 +0800

    提交sql脚本

---
+++ sql/create_table.sql
@@ -0,0 +1,148 @@
+CREATE TABLE `t_cfg_dict` (
+  `dict_id` int(5) NOT NULL AUTO_INCREMENT COMMENT '主键标识',
+  `dict_code` int(5) NOT NULL COMMENT '枚举值id',
+  `col_name` varchar(20) NOT NULL COMMENT '枚举值类型',
+  `dict_note` varchar(20) NOT NULL COMMENT '对应的枚举值',
+  `remark` varchar(100) NOT NULL COMMENT '其他说明信息',
+  PRIMARY KEY (`dict_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='配置表';
+
+CREATE TABLE `t_cfg_menu` (
+  `id` int(5) NOT NULL AUTO_INCREMENT COMMENT '菜单id',
+  `name` varchar(50) NOT NULL COMMENT '菜单名称',
+  `parent_id` int(5) NOT NULL COMMENT '上级菜单id',
+  `url` varchar(255) DEFAULT NULL COMMENT 'url链接',
+  `style` varchar(50) DEFAULT NULL COMMENT '样式',
+  `sort` int(5) DEFAULT NULL COMMENT '排序',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='菜单表';
+
+CREATE TABLE `t_cfg_role` (
+  `id` varchar(40) NOT NULL COMMENT '角色id',
+  `name` varchar(40) NOT NULL COMMENT '角色名称',
+  `description` varchar(50) DEFAULT NULL COMMENT '角色描述',
+  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_role_name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='角色表';
+
+CREATE TABLE `t_cfg_rolemenu` (
+  `role_id` varchar(40) NOT NULL DEFAULT '' COMMENT '角色id',
+  `menu_id` int(5) NOT NULL DEFAULT '0' COMMENT '菜单id',
+  PRIMARY KEY (`role_id`,`menu_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='角色菜单关联表';
+
+CREATE TABLE `t_cfg_user` (
+  `id` varchar(40) NOT NULL COMMENT '主键,用户id',
+  `customer_id` varchar(40) NOT NULL COMMENT '所属客户id',
+  `user_name` varchar(20) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL COMMENT '用户名',
+  `password` varchar(50) NOT NULL COMMENT '密码',
+  `true_name` varchar(50) NOT NULL COMMENT '真实姓名',
+  `phone` varchar(20) NOT NULL COMMENT '手机号',
+  `email` varchar(50) DEFAULT NULL COMMENT '邮箱',
+  `user_type` int(5) NOT NULL COMMENT '用户类型 1:系统管理员;2:客户管理员;3:普通客户用户',
+  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` datetime DEFAULT NULL COMMENT '修改时间',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_user_name` (`user_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户表';
+
+CREATE TABLE `t_cfg_userrole` (
+  `user_id` varchar(40) NOT NULL DEFAULT '' COMMENT '用户id',
+  `role_id` varchar(40) NOT NULL DEFAULT '' COMMENT '角色id',
+  PRIMARY KEY (`user_id`,`role_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户角色关联表';
+
+CREATE TABLE `b_define` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `b_code` varchar(20) NOT NULL COMMENT '编码',
+  `b_id` varchar(5) NOT NULL COMMENT '楼宇ID',
+  `o_id` varchar(100) DEFAULT NULL COMMENT '管理员ID',
+  `des` varchar(100) DEFAULT NULL COMMENT '描述',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uk_b_define_id` (`b_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='楼宇表';
+
+CREATE TABLE `f_define` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `b_id` varchar(5) NOT NULL COMMENT '楼宇ID',
+  `f_code` varchar(100) DEFAULT NULL,
+  `f_id` varchar(10) NOT NULL COMMENT '楼层ID',
+  `o_id` varchar(100) DEFAULT NULL COMMENT '管理员ID',
+  `des` varchar(100) DEFAULT NULL COMMENT '描述',
+  PRIMARY KEY (`id`),
+  KEY `idx_f_define_id` (`b_id`,`f_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='楼层定义表';
+
+CREATE TABLE `r_define` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `b_id` varchar(5) NOT NULL COMMENT '楼宇ID',
+  `f_id` varchar(10) NOT NULL COMMENT '楼层ID',
+  `r_id` varchar(15) NOT NULL COMMENT '房间ID',
+  `o_id` varchar(100) DEFAULT NULL COMMENT '管理员ID',
+  `des` varchar(100) DEFAULT NULL COMMENT '描述',
+  PRIMARY KEY (`id`),
+  KEY `idx_r_define_id` (`b_id`,`f_id`,`r_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='房间定义表';
+
+CREATE TABLE `c_define` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `c_code` varchar(20) DEFAULT NULL COMMENT '控制器code',
+  `c_id` varchar(20) NOT NULL COMMENT '控制器Id',
+  `s_code` varchar(20) DEFAULT NULL COMMENT '传感器code',
+  `s_id` varchar(20) NOT NULL COMMENT '传感器Id',
+  `c_type` tinyint(1) NOT NULL COMMENT '控制器类型:1.新风机,2.VAV,3.通风柜,4.H2探头',
+  `s_type` tinyint(1) NOT NULL COMMENT '传感器类型:1 压差传感器,2 温湿,3 湿度,4 通风柜门高,5 人员工作状态,6 面风速,7 余风量,8 变风量送风蝶阀风量',
+  `s_max` float DEFAULT NULL COMMENT '上门限值',
+  `s_min` float DEFAULT NULL COMMENT '下门限值',
+  `c_out` JSON DEFAULT NULL COMMENT '下发指令集',
+  `alarm_type` int(1) NOT NULL COMMENT '是否告警：0不告警，1告警',
+  `code_value_min` int(20) NOT NULL COMMENT '码值最小',
+  `code_value_max` int(20) NOT NULL COMMENT '码值最大',
+  `project_value_min` int(2) NOT NULL COMMENT '工程值最小',
+  `project_value_max` int(2) NOT NULL COMMENT '工程值最大',
+  `offsett` int(20) NOT NULL COMMENT '偏移量',
+  `unit_item` varchar(2) NOT NULL COMMENT '界面单位',
+  `des` varchar(200) DEFAULT NULL COMMENT '描述',
+  `m_t_address` smallint(5) NOT NULL COMMENT 'modbus tcp寄存器地址',
+  `m_t_length` tinyint(4) NOT NULL COMMENT 'modbus tcp长度',
+  `m_t_collect` tinyint(1) DEFAULT NULL COMMENT 'modbus tcp是否采集该设备',
+  `s_pid` varchar(20) NOT NULL COMMENT '传感器集合Id',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uk_c_define_m_t_address` (`m_t_address`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='控制器定义表';
+
+CREATE TABLE `e_list` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `c_id` varchar(20) NOT NULL COMMENT '控制器Id',
+  `p_type` tinyint(1) NOT NULL COMMENT '协议类型：1 Modbus TCP,2 TCP,3 UDP',
+  `m_t_port` smallint(6) NOT NULL COMMENT 'modbus tcp使用的端口',
+  `m_t_ip` varchar(15) NOT NULL COMMENT 'modbus tcp使用的ip',
+  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '设备添加时间',
+  `status` tinyint(1) NOT NULL DEFAULT '0' COMMENT '状态：0 正常,1 异常,2 手工离线',
+  `o_id` varchar(100) DEFAULT NULL COMMENT '管理员ID',
+  `des` varchar(200) DEFAULT NULL COMMENT '描述',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uk_e_list_c_id` (`c_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=utf8 COMMENT='系统信息表';
+
+CREATE TABLE `s_status` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `c_id` varchar(20) NOT NULL COMMENT '控制器Id',
+  `s_id` varchar(20) NOT NULL COMMENT '传感器Id',
+  `lastupdatetime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '最后状态更新时间',
+  `s_flag` varchar(2) DEFAULT '-1' COMMENT '开关类传感器状态：-1 未使用,0 打开,1 关闭',
+  `m_t_address` smallint(5) NOT NULL COMMENT 'modbus tcp寄存器地址',
+  `m_t_length` tinyint(4) NOT NULL COMMENT 'modbus tcp长度',
+  `alarm_type` int(1) NOT NULL COMMENT '是否告警：0不告警，1告警',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uk_s_status_m_t_address` (`m_t_address`)  
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='开关量状态数据表';
+
+CREATE TABLE `e_data` (
+  `s_id` varchar(20) NOT NULL COMMENT '传感器Id',
+  `input_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '上报时间',
+  `value` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='控制器数据表';
+
+
---
+++ sql/init_data.sql
@@ -0,0 +1,46 @@
+INSERT INTO t_cfg_user(id, customer_id, user_name, password, true_name, phone, user_type)
+values('b7d0a1cff9e511e7bf401866daf21a53','0000', 'admin', '21218cca77804d2ba1922c33e0151105', '超级管理员', '0000', 1);
+
+
+INSERT INTO `t_cfg_menu` (`id`, `name`, `parent_id`, `url`, `style`, `sort`) VALUES (10000, '设备', 0, '/deviceQry', '', 1);
+INSERT INTO `t_cfg_menu` (`id`, `name`, `parent_id`, `url`, `style`, `sort`) VALUES (20000, '风机盘管', 0, '/fanCoiler', '', 1);
+INSERT INTO `t_cfg_menu` (`id`, `name`, `parent_id`, `url`, `style`, `sort`) VALUES (30000, '报表', 0, '/report', '', 1);
+INSERT INTO `t_cfg_menu` (`id`, `name`, `parent_id`, `url`, `style`, `sort`) VALUES (40000, '历史曲线', 0, '/hisChart', '', 1);
+INSERT INTO `t_cfg_menu` (`id`, `name`, `parent_id`, `url`, `style`, `sort`) VALUES (50000, '告警', 0, '/alarm', '', 1);
+
+truncate table t_cfg_rolemenu;
+INSERT INTO `t_cfg_rolemenu` VALUES ('0093f579f9e711e7bf401866daf21a53', 10000);
+INSERT INTO `t_cfg_rolemenu` VALUES ('0093f579f9e711e7bf401866daf21a53', 20000);
+INSERT INTO `t_cfg_rolemenu` VALUES ('0093f579f9e711e7bf401866daf21a53', 30000);
+INSERT INTO `t_cfg_rolemenu` VALUES ('0093f579f9e711e7bf401866daf21a53', 40000);
+INSERT INTO `t_cfg_rolemenu` VALUES ('0093f579f9e711e7bf401866daf21a53', 50000);
+
+
+truncate table t_cfg_dict;
+INSERT INTO `t_cfg_dict` (`dict_id`, `dict_code`, `col_name`, `dict_note`, `remark`) VALUES (11, 1, 'sensor_type', '压差', 'pressure');
+INSERT INTO `t_cfg_dict` (`dict_id`, `dict_code`, `col_name`, `dict_note`, `remark`) VALUES (12, 2, 'sensor_type', '温湿度', 'tempAndHum');
+INSERT INTO `t_cfg_dict` (`dict_id`, `dict_code`, `col_name`, `dict_note`, `remark`) VALUES (13, 3, 'sensor_type', '温度', 'temperature');
+INSERT INTO `t_cfg_dict` (`dict_id`, `dict_code`, `col_name`, `dict_note`, `remark`) VALUES (14, 4, 'sensor_type', '湿度', 'humidity');
+INSERT INTO `t_cfg_dict` (`dict_id`, `dict_code`, `col_name`, `dict_note`, `remark`) VALUES (15, 5, 'sensor_type', '通风柜', 'ventilator');
+INSERT INTO `t_cfg_dict` (`dict_id`, `dict_code`, `col_name`, `dict_note`, `remark`) VALUES (16, 6, 'sensor_type', '通风柜门高', 'ventilator');
+INSERT INTO `t_cfg_dict` (`dict_id`, `dict_code`, `col_name`, `dict_note`, `remark`) VALUES (17, 7, 'sensor_type', '人员工作状态', 'workState');
+INSERT INTO `t_cfg_dict` (`dict_id`, `dict_code`, `col_name`, `dict_note`, `remark`) VALUES (18, 8, 'sensor_type', '面风速', 'airSpeed');
+INSERT INTO `t_cfg_dict` (`dict_id`, `dict_code`, `col_name`, `dict_note`, `remark`) VALUES (19, 9, 'sensor_type', '通风柜变风量排风阀风量', 'ventilator_airVolumn');
+INSERT INTO `t_cfg_dict` (`dict_id`, `dict_code`, `col_name`, `dict_note`, `remark`) VALUES (20, 10, 'sensor_type', '余风量', 'airVolumn');
+INSERT INTO `t_cfg_dict` (`dict_id`, `dict_code`, `col_name`, `dict_note`, `remark`) VALUES (21, 11, 'sensor_type', '房间排风量', 'room_out_airVolumn');
+INSERT INTO `t_cfg_dict` (`dict_id`, `dict_code`, `col_name`, `dict_note`, `remark`) VALUES (22, 12, 'sensor_type', '房间送风量', 'room_in_airVolumn');
+INSERT INTO `t_cfg_dict` (`dict_id`, `dict_code`, `col_name`, `dict_note`, `remark`) VALUES (23, 13, 'sensor_type', '变风量送风蝶阀风量', 'flygate');
+
+INSERT INTO `t_cfg_dict` (`dict_id`, `dict_code`, `col_name`, `dict_note`, `remark`) VALUES (31, 31, 'sensor_type', '可编程控制器', 'programmable_controller');
+INSERT INTO `t_cfg_dict` (`dict_id`, `dict_code`, `col_name`, `dict_note`, `remark`) VALUES (32, 32, 'sensor_type', '管道静压传感器', 'pipe_pressure');
+INSERT INTO `t_cfg_dict` (`dict_id`, `dict_code`, `col_name`, `dict_note`, `remark`) VALUES (33, 33, 'sensor_type', '流量开关', 'flow_switch');
+INSERT INTO `t_cfg_dict` (`dict_id`, `dict_code`, `col_name`, `dict_note`, `remark`) VALUES (34, 34, 'sensor_type', '管道温湿度传感器', 'pipe_tempAndHum');
+INSERT INTO `t_cfg_dict` (`dict_id`, `dict_code`, `col_name`, `dict_note`, `remark`) VALUES (35, 35, 'sensor_type', '气流开关', 'airflow_switch');
+INSERT INTO `t_cfg_dict` (`dict_id`, `dict_code`, `col_name`, `dict_note`, `remark`) VALUES (36, 36, 'sensor_type', '防冻开关', 'freezeprotect_switch');
+INSERT INTO `t_cfg_dict` (`dict_id`, `dict_code`, `col_name`, `dict_note`, `remark`) VALUES (37, 37, 'sensor_type', '冷热水调节阀', 'regulating_valve');
+INSERT INTO `t_cfg_dict` (`dict_id`, `dict_code`, `col_name`, `dict_note`, `remark`) VALUES (38, 38, 'sensor_type', '电动风阀执行器', 'airvalve_actuator');
+INSERT INTO `t_cfg_dict` (`dict_id`, `dict_code`, `col_name`, `dict_note`, `remark`) VALUES (39, 39, 'sensor_type', '排风变频器', 'frequency_converter');
+INSERT INTO `t_cfg_dict` (`dict_id`, `dict_code`, `col_name`, `dict_note`, `remark`) VALUES (40, 40, 'sensor_type', '新风变频器', 'frequency_converter');
+
+
+
